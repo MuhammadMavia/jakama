16c641244b349971a646c83a94b211debbdbb3f5
Muhammad Mavia <MuhammadMavia172@gmail.com>
2016-05-06 12:04:18 +0500

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,24 @@
+angular.module('starter', ['ionic'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('page01', {
+        url: '/page01',
+        templateUrl: 'templates/page01.html'
+      });
+
+    $urlRouterProvider.otherwise('/page01');
+
+  });
