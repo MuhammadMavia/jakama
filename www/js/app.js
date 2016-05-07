bc36fc02b2c5253dece3ed67feab12ee46cd6e05
Muhammad Mavia <MuhammadMavia172@gmail.com>
2016-05-07 17:20:09 +0500

    done

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 angular.module('starter', ['ionic'])
 
-  .run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform, $rootScope) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -9,19 +9,20 @@ angular.module('starter', ['ionic'])
       if (window.StatusBar) {
         StatusBar.styleDefault();
       }
+      $rootScope.item = {};
+      $rootScope.$on('$stateChangeStart',
+        function () {
+          $rootScope['bottle'] = $rootScope.item.bottle;
+          $rootScope['sock'] = $rootScope.item.sock;
+        })
     });
   })
-
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('menu', {
         url: '/menu',
         templateUrl: 'templates/menu.html'
       })
-      /*.state('page02', {
-       url: '/page02',
-       templateUrl: 'templates/app.html'
-       })*/
       .state('app', {
         url: '/app',
         abstract: true,
@@ -46,15 +47,27 @@ angular.module('starter', ['ionic'])
       });
 
 
-    $urlRouterProvider.otherwise('/app/page01');
+    $urlRouterProvider.otherwise('/menu');
 
   })
-  .controller('AppCtrl', function ($scope) {
+  .controller('AppCtrl', function ($scope, $rootScope) {
     $scope.exit = function () {
       ionic.Platform.exitApp()
     };
-    $scope.bottlePosition = true;
-    $scope.bottlePutInBox = function () {
-      $scope.bottlePosition = false;
-    }
+    $scope.itemPutInBox = function (itemName, boxIndex) {
+      $rootScope.item[itemName] = true;
+      var box = document.getElementById('box-' + boxIndex);
+      var item = document.getElementById(itemName);
+      item.removeAttribute('ng-click');
+      item.removeAttribute('class');
+      item.setAttribute('class', 'item-on-box');
+      box.appendChild(item);
+    };
+    $scope.orangeBoxActive = function (i) {
+      var box = document.getElementById('box-' + i);
+      if (box.childElementCount) {
+        box.classList == 'orange-border activated' ? box.className = 'orange-border-hide' : box.className = 'orange-border';
+
+      }
+    };
   });
